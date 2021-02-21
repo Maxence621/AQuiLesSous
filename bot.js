af94927fef1a9a03116c2f838546f613a28d1c43
Maxence621 <48748900+Maxence621@users.noreply.github.com>
2021-02-21 02:42:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,16 @@
-const Discord = require('discord.js');
+const Promise = require('bluebird')
+const AppDAO = require('./dao')
+const TaskRepository = require('./task_repository')
+const dao = new AppDAO('./database.sqlite3')
+const taskRepo = new TaskRepository(dao)
+let taskId
+var string ="";
+var pseudo ="";
+var sous ="";
 
+
+
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
  
@@ -21,6 +32,44 @@ client.on('message', message => {
        }
 
 });
+
+client.on('message', message => {
+
+    if (message.content === '!bluehelp') {
+
+       message.reply('> ***Bonjour toi !*** voila l\'aide que tu m\'a demander :\n > \n > "**!commandes**" : Permet de voir les commandes disponibles\n > "**!ajouterDette <Utilisateur> <dette>**" : Permet d\'ajouter une dette\n > "**!afficherDette**" : Permet de voir les dettes à rembourser');
+
+       }
+
+});
+
+client.on('message', message => {
+
+    if (message.content === '!afficherDette') {
+     string+='> **Voici la liste de tes dettes !**\n '
+     taskRepo.createTable()
+        .then(() => taskRepo.getAll())
+        .then((tasks) => {
+          return new Promise((resolve, reject) => {
+            tasks.forEach((task) => {
+                var pseudo = (`${task.name}`)
+                var dette = (`${task.dette}`)
+
+                
+                
+                string+='> '+pseudo+'         ***Dette : ***'+dette+'€\n ';
+
+
+            })
+     
+
+       message.reply(string);
+
+       }
+
+});
+
+
 
 client.on('message', message => {
 
