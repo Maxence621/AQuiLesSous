b2742a076c3999483c2e2a6aa0750fa27a06d939
Maxence621 <48748900+Maxence621@users.noreply.github.com>
2021-02-21 23:30:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
-const Discord = require('discord.js');
 const fs = require('fs');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-
 
 
  
@@ -30,41 +29,96 @@ client.on('message', message => {
 
        message.reply('> ***Bonjour toi !*** voila l\'aide que tu m\'a demander :\n > \n > "**!bluehelp**" : Permet de voir les commandes disponibles\n > "**!ajouterDette <Utilisateur> <dette>**" : Permet d\'ajouter une dette\n > "**!afficherDette**" : Permet de voir les dettes à rembourser');
 
-    }else if (message.content === '!ajouterDette') {
-     
-     var Donnee = message.split(' ');
-        Auteur = Donnee[1];
-        Dette = Donnee[2];
-        Pseudo = Donnee[3];
-     
-     if (idUtilisateur!=null){
-        idUtilisateur +=1
-      }else{
-        var Utilisateur=0;
-      }
-     
+    }
+
+});
+
+
+const prefix = '!'; 
+
+client.on('message', message => {
+  var fs = require('fs');
+  var IdAuteur = message.author.id;
+
+  if (!message.content.startsWith(prefix)) return;
+  const args = message.content.trim().split(/ +/g);
+  const cmd = args[0].slice(prefix.length).toLowerCase(); 
+
+  if (cmd === 'ajouterdette') {
+    if (!args[1]) return message.reply('Veuillez ajouter des arguments !');
+    if (args[4]) return message.reply('\n > Tu a mis trop d\'arguments !\n > "!ajouterdette <Dette> <Pseudo>" ');
+
+
+    console.log(message.author.id);
+    
+    var fs = require('fs')
      fs.readFile('dette.json', 'utf-8', function(err, data) {
        if (err) throw err
 
-     var arrayOfObjects = JSON.parse(data)
+     var arrayOfObjects = JSON.parse(data);
+  var Taille = Object.keys(arrayOfObjects.users).length;
      arrayOfObjects.users.push({
-       id:idUtilisateur,
-       auteur: Auteur,
-       dette: Dette,
-       pseudo: Pseudo
+       auteur: IdAuteur,
+       dette: args[1],
+       pseudo: args[2]
      })
 
-  console.log(arrayOfObjects.users[0])
+     
+     if(Taille==Object.keys(arrayOfObjects.users).length){
+      message.channel.send("une Erreur est survenue, veuillez réessayer ! ");
+     }else{
+      message.channel.send("Tu as bien ajouté cette dette !");
+     }
 
-  fs.writeFile('dette.json', JSON.stringify({arrayOfObjects},null ,3), 'utf-8', function(err) {
+
+    
+
+
+  fs.writeFile('./dette.json', JSON.stringify(arrayOfObjects, null, 2), 'utf-8', function(err) {
     if (err) throw err
     console.log('Done!')
   })
 })
-    
+   
     }
 
 });
+
+
+
+
+
+client.on('message', message => {
+
+    if (message.content === '!afficherdette') {
+      var fs = require('fs');
+      var IdAuteur = message.author.id;
+      var string= "> Voici la liste de tes dette "+"<@"+IdAuteur+"> ! \n";
+
+       fs.readFile('dette.json', 'utf-8', function(err, data) {
+       if (err) throw err
+      var arrayOfObjects = JSON.parse(data);
+      var Taille = Object.keys(arrayOfObjects.users).length;
+  
+        for (var i = 0; i < Taille; i++) {
+             if(IdAuteur == arrayOfObjects.users[i].auteur){
+
+              string += "> ***Dettes :*** "+arrayOfObjects.users[i].dette+"\n > ***à remboursé à : ***"+arrayOfObjects.users[i].pseudo+"\n\n ";
+             }else{
+              console.log("Boucle i :"+i);
+              console.log("Taille :"+Taille);
+             }
+        }
+        message.channel.send(string);
+        
+       })
+    }
+});
+
+
+
+
+
 
 
 
