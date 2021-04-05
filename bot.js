4ceabb67b799177120f0b0f61b15a74adbedec3b
Maxence621 <48748900+Maxence621@users.noreply.github.com>
2021-04-05 15:15:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -207,7 +207,7 @@ bot.on('message', message => {
 
 		var pseudo_dette1 = args[1];
 		
-		var pseudo_dette = pseudo_dette1.substring(3, pseudo_dette1.length-1);
+		var pseudo_dette = pseudo_dette1.substring(2, pseudo_dette1.length-1);
 		
 		var correct0="0";
 		
