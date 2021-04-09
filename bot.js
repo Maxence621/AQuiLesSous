129b08ee15dac62834c466518a34c2ed17af2b6c
Maxence621 <48748900+Maxence621@users.noreply.github.com>
2021-04-09 18:08:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ const client = new Client({
 
 client.connect();
 
-/*
+
 
 
 
@@ -439,6 +439,4 @@ bot.on('message', message => {
 // THIS  MUST  BE  THIS  WAY
 
 bot.login(process.env.BOT_TOKEN);//BOT_TOKEN is the bot Secret
-
-*/
 
